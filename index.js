50899ac49b6c0fc15040d7ca6b5d2f8c2260ff23
ngoc_linh <dinhthingoclinh92@gmail.com>
2023-05-14 00:12:15 +0700

    update

--- index.js
+++ index.js
@@ -1,3 +1,52 @@
+// B1: Tim so nguyen duong nho nhat sao cho 1+2+3...+n>10000
+// input : Nhập số n (kiểu dữ kiệu số)
+// xử lý: chay while thoả đk 1+2+3...+n>10000
+// output: xuất kq
+
+function timSoNguyenDuongN() {
+    //xử lý
+    var ketQua = 0;
+    var i = 1;
+    while (ketQua < 10000) {   
+        ketQua += i;
+        i++; // tăng i lên nếu không sẽ bị lặp vô hạn
+    }
+// console.log(ketQua);
+// console.log(i);
+    //xuất ra màn hình
+    document.querySelector(".soNguyenDuong").innerHTML = "Số nguyên dương nhỏ nhất thoả điều kiện là: " + i;
+}
+
+
+// B2: tính tong
+// input : Nhập số x,n (kiểu dữ kiệu số)
+// xử lý: viet ham luy thua, chay vong lap
+// output: xuất kq
+function hamLuyThua(x,soMu) {
+    var ketQua = 1;
+    for (var i = 1; i<=soMu; i++)
+    {
+        ketQua = ketQua * x;
+    }
+    return ketQua;
+}
+function tinhTong() {
+    //lấy dữ liệu từ người dùng
+    var soMu = document.getElementById("nhapSoN").value * 1;
+    var soX= document.getElementById("nhapSoX").value * 1;
+    //xử lý
+    var ketQua = 0;
+    // ketQua = hamLuyThua(soX,soMu);
+    
+    for (var i = 1; i <=soMu; i++)
+    {
+        ketQua += hamLuyThua(soX,i);
+        // console.log(ketQua);
+    }
+    
+    //xuất ra màn hình
+    document.querySelector(".tinhTong").innerHTML = "Tổng luỹ thừa là: " + ketQua;
+}
 
 
 // B3: tính giai thừa
@@ -39,9 +88,52 @@ function in10TheDiv() {
             document.querySelector(".inTheDiv").appendChild(node).style.background ="green" ;
         } else {
             document.querySelector(".inTheDiv").appendChild(node).style.background ="red" ;
+        } 
+    }
+}
+// B5: in so nguyen to
+// input : Nhập số n (kiểu dữ kiệu số)
+// xử lý: ket qua =in so nguyen to
+// output: xuất kq
+function kiem_tra_snt(n)
+{
+    // Biến cờ hiệu
+    var flag = true;
+ 
+    // Nếu n bé hơn 2 tức là không phải số nguyên tố
+    if (n < 2){
+        flag = false;
+    }
+    else{
+        // lặp từ 2 tới n-1
+        for (var i = 2; i < n-1; i++)
+        {
+            if (n % i == 0){
+                flag = false;
+                break;
+            }
         }
-        
-        
+    }
+ 
+    // Kiểm tra biến flag
+    if (flag == true){
+        return flag;
+    }
+}
+function inSoNguyenTo() {
+    //lấy dữ liệu từ người dùng
+    var giaiTriNhap = document.getElementById("giaiTriNhap").value * 1;
+
+    //xử lý
+    var ketQua = "";
+    for (var i =1; i <=giaiTriNhap; i++)
+    {
+        if (kiem_tra_snt(i) == true)
+        {
+            ketQua = ketQua + i + " ";
+        }
     }
 
+    //xuất ra màn hình
+    document.querySelector(".inSoNguyenTO").innerHTML = "Các số nguyên tố là: " + ketQua;
 }
